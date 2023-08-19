305c7ec7f07f011e8da17f8d27633991295b0a5f
Sega0602 <smorokov2017@gmail.com>
2023-08-20 01:07:18 +0300

    addPingPong

--- po.c
+++ po.c
@@ -15,9 +15,167 @@ void printTable(int leftR, int rightR, int ball) {
          }
          }
          printf("\n");
-     
-      sergey test
+     }
+    }
+    int playerOneTurn(char key, int currentPosition) {
+        if (key == 'a') {
+            if (currentPosition > 2) {
+                currentPosition--;
+            }
+            } else if (key == 'z') {
+            if (currentPosition < 22) {
+                currentPosition++;
+            }
+    }
+        return currentPosition;
+}
+int playerTwoTurn(char key, int currentPosition) {
+        if (key == 'k') {
+            if (currentPosition > 2) {
+                currentPosition--;
+            }
+            } else if (key == 'm') {
+            if (currentPosition < 22) {
+                currentPosition++;
+            }
+    }
+        return currentPosition;
+}
 
-      init van 
+int main() {
+    int leftR = 12;
+    int rightR = 12;
+    int ball = 1;
+    int prevBall;
+    int playerOneScore = 0;
+    int playerTwoScore = 0;
+
+    while (playerOneScore <= 21 || playerTwoScore <= 21) {
+        if (playerOneScore == 21) {
+            printf("Player 1 win! Congratulations!");
+            break;
+        } else if (playerTwoScore == 21) {
+            printf("Player 2 win! Congratulations!");
+            break;
+        }
+
+       char key;
+        key = getchar();
+        if (key == 'a' || key == 'z') {
+        leftR = playerOneTurn(key, leftR);
+        if (ball == 1 && (leftR == 11 || leftR == 12 || leftR ==13)) {
+        prevBall = ball;
+        ball++;
+        printTable(leftR, rightR, ball);
+    } else if (ball == 78 && (rightR == 11 || rightR == 12 || rightR ==13)) {
+        prevBall = ball;
+        ball--;
+        printTable(leftR, rightR, ball);
+    } else if (ball == 1 && (leftR != 11 && leftR != 12 && leftR != 13)) {
+        playerTwoScore++;
+        printf("Player 1: %d\nPlayer 2: %d\n", playerOneScore, playerTwoScore);
+        ball = 1;
+        prevBall = 0;
+        leftR = 12;
+        rightR = 12;
+    } else if (ball == 78 && (rightR != 11 && rightR != 12 && rightR != 13)) {
+        playerOneScore++;
+        printf("Player 1: %d\nPlayer 2: %d\n", playerOneScore, playerTwoScore);
+        ball = 78;
+        prevBall = 0;
+        leftR = 12;
+        rightR = 12;
+    } else if (ball > 1 && ball < 78) {
+        if (prevBall < ball) {
+                prevBall = ball;
+                ball++;
+                printTable(leftR, rightR, ball);
+            } else if (prevBall > ball) {
+                prevBall = ball;
+                ball--;
+                printTable(leftR, rightR, ball);
+    }
+    } else {
+        printf("Ball motion mistake!\n");
+    }
+    } else if (key == 'k' || key == 'm') {
+        rightR = playerTwoTurn(key, rightR); {
+    if (ball == 1 && (leftR == 11 || leftR == 12 || leftR ==13)) {
+        prevBall = ball;
+        ball++;
+        printTable(leftR, rightR, ball);
+    } else if (ball == 78 && (rightR == 11 || rightR == 12 || rightR ==13)) {
+        prevBall = ball;
+        ball--;
+        printTable(leftR, rightR, ball);
+    } else if (ball == 1 && (leftR != 11 && leftR != 12 && leftR != 13)) {
+        playerTwoScore++;
+        printf("Player 1: %d\nPlayer 2: %d\n", playerOneScore, playerTwoScore);
+        ball = 1;
+        prevBall = 0;
+        leftR = 12;
+        rightR = 12;
+    } else if (ball == 78 && (rightR != 11 && rightR != 12 && rightR != 13)) {
+        playerOneScore++;
+        printf("Player 1: %d\nPlayer 2: %d\n", playerOneScore, playerTwoScore);
+        ball = 78;
+        prevBall = 0;
+        leftR = 12;
+        rightR = 12;
+    } else if (ball > 1 && ball < 78) {
+        if (prevBall < ball) {
+                prevBall = ball;
+                ball++;
+                printTable(leftR, rightR, ball);
+            } else if (prevBall > ball) {
+                prevBall = ball;
+                ball--;
+                printTable(leftR, rightR, ball);
+    }
+    } else {
+        printf("Ball motion mistake!\n");
+    }
+}
+} else if (key == ' ') {
+    if (ball == 1 && (leftR == 11 || leftR == 12 || leftR ==13)) {
+        prevBall = ball;
+        ball++;
+        printTable(leftR, rightR, ball);
+    } else if (ball == 78 && (rightR == 11 || rightR == 12 || rightR ==13)) {
+        prevBall = ball;
+        ball--;
+        printTable(leftR, rightR, ball);
+    } else if (ball == 1 && (leftR != 11 && leftR != 12 && leftR != 13)) {
+        playerTwoScore++;
+        printf("Player 1: %d\nPlayer 2: %d\n", playerOneScore, playerTwoScore);
+        ball = 1;
+        prevBall = 0;
+        leftR = 12;
+        rightR = 12;
+    } else if (ball == 78 && (rightR != 11 && rightR != 12 && rightR != 13)) {
+        playerOneScore++;
+        printf("Player 1: %d\nPlayer 2: %d\n", playerOneScore, playerTwoScore);
+        ball = 78;
+        prevBall = 0;
+        leftR = 12;
+        rightR = 12;
+    } else if (ball > 1 && ball < 78) {
+        if (prevBall < ball) {
+                prevBall = ball;
+                ball++;
+                printTable(leftR, rightR, ball);
+            } else if (prevBall > ball) {
+                prevBall = ball;
+                ball--;
+                printTable(leftR, rightR, ball);
+    }
+    } else {
+        printf("Ball motion mistake!\n");
+    }
+} else if (key == 'q') {
+        printf("Player 1: %d\nPlayer 2: %d\n", playerOneScore, playerTwoScore);
+        break;
+}
+}
     }
 
