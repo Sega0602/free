e7256ade3ba6cc0bc80825c35ad4024cbd90eb55
Sega0602 <smorokov2017@gmail.com>
2023-08-20 02:18:24 +0300

    addPongen

---
+++ resours/pongen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void printTable(int leftR, int rightR, int ball) {
+     for (int height = 0; height < 25; height++)  {  // height - высота поля
+         for (int width = 0; width < 80; width++) {   // width - ширина поля
+         if (height == 0 || height == 24) {
+             printf("-");
+         } else if (((height == leftR || height == leftR+1 || height == leftR - 1) && width == 0) ||
+         ((height == rightR ||height == rightR+1 || height == rightR-1) && width == 79)) {
+         printf("|");
+         } else  if (height == 12 && width == ball) {
+             printf("o");
+             } else {
+             printf(" ");
+         }
+         }
+         printf("\n");
+     
+    }
+
